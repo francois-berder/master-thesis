8542baf96c731be626410cba755c4ecf7bfbbde3
Francois Berder <fberder@hotmail.fr>
2015-09-01 15:25:02 +0200

    plot model script refactored + deleted unused model images.

--- plotmodel.py
+++ plotmodel.py
@@ -4,11 +4,45 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import math
 
-def funcSecondOrder(x, a, b, c):
+def funcQuadratic(x, a, b, c):
     return a + np.array(x) * b + c * np.square(x)
 
 def funcLinear(x, a, b):
     return a + np.array(x) * b;
+
+def findModel(name, f, datat, datax, datay):
+    poptX, pcovX = curve_fit(f, datat, datax)
+    poptY, pcovY = curve_fit(f, datat, datay)
+
+    print(name + ' model:\n')
+    print(poptX)
+    print(poptY)
+    print('\n')
+
+    xs = np.linspace(0., 0.4, 200)
+    ys = []
+    for x in xs:
+        ys.append(f([x], *poptX))
+
+    ys2 = []
+    for x in xs:
+        ys2.append(f([x], *poptY))
+
+    plt.figure()
+    plt.xlabel('bending angle', fontsize=18)
+    plt.ylabel('x', fontsize=18)
+    plt.scatter(datat, datax)
+    plt.plot(xs, ys, 'r')
+    plt.savefig('model-' + name + '-x.png')
+    plt.savefig('report/images/model-' + name + '-x.png')
+
+    plt.figure()
+    plt.xlabel('bending angle', fontsize=18)
+    plt.ylabel('y', fontsize=18)
+    plt.scatter(datat, datay)
+    plt.plot(xs, ys2, 'r')
+    plt.savefig('model-' + name + '-y.png')
+    plt.savefig('report/images/model-' + name + '-y.png')
 
 if __name__ == '__main__':
 
@@ -27,33 +61,6 @@ if __name__ == '__main__':
 
     tmp = list(zip(*data))
 
-    # Find 
-    poptX, pcovX = curve_fit(funcSecondOrder, tmp[0], tmp[1])
-    poptY, pcovY = curve_fit(funcSecondOrder, tmp[0], tmp[2])
-    print(poptX)
-    print(poptY)
+    findModel("linear", funcLinear, tmp[0], tmp[1], tmp[2])
+    findModel("quadratic", funcQuadratic, tmp[0], tmp[1], tmp[2])
 
-    xs = np.linspace(0., 0.4, 200)
-    ys = []
-    for x in xs:
-        ys.append(funcSecondOrder([x], *poptX))
-
-    ys2 = []
-    for x in xs:
-        ys2.append(funcSecondOrder([x], *poptY))
-
-    plt.figure()
-    plt.xlabel('bending angle', fontsize=18)
-    plt.ylabel('x', fontsize=18)
-    plt.scatter(tmp[0], tmp[1])
-    plt.plot(xs, ys, 'r')
-    plt.savefig('data/modelx.png')
-
-    plt.figure()
-    plt.xlabel('bending angle', fontsize=18)
-    plt.ylabel('y', fontsize=18)
-    plt.scatter(tmp[0], tmp[2])
-    plt.plot(xs, ys2, 'r')
-    plt.savefig('data/modely.png')
-
-    plt.show()
